dcad77cff16aaf014cce836c76aefdf1641e11c1
André Calazans <andcalazans@hotmail.com>
2022-11-27 22:48:13 -0300

    Finish quote app

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,9 @@ User.create(
 Quote.create(company: kpmg, name: 'First quote')
 Quote.create(company: kpmg, name: 'Second quote')
 Quote.create(company: kpmg, name: 'Third quote')
+
+
+LineItem.create(line_item_date: LineItemDate.last, name: 'Meeting room', description: 'A cosy meeting room for 10 people', quantity: '1', unit_price: '1000')
+LineItem.create(line_item_date: LineItemDate.last, name: 'Meal tray', description: 'Our delicious meal tray', quantity: '10', unit_price: '25')
+LineItem.create(line_item_date: LineItemDate.last, name: 'Meeting room', description: 'A cosy meeting room for 10 people', quantity: '1', unit_price: '1000')
+LineItem.create(line_item_date: LineItemDate.last, name: 'Meal tray', description: 'Our delicious meal tray', quantity: '10', unit_price: '25')
